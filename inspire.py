814f03561a2b3f3342245cb7e7df44b97d9af1de
guillem <gbuendia@users.noreply.github.com>
2017-06-30 08:36:15 +0200

    Change getimage to use requests instead of urllib

--- inspire.py
+++ inspire.py
@@ -11,6 +11,7 @@ import sys
 import io
 from urllib.request import Request, urlopen
 # Apparently PIL doesn't work in Python3
+# so pillow has to be installed first
 from PIL import Image, ImageTk
 
 def inspire():
@@ -18,18 +19,19 @@ def inspire():
 	headerDict = {
 		"User-Agent" : "Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19" 
 	}
-	resultURL = requests.get("http://inspirobot.me/api?generate=true",stream=False,headers=headerDict).text+"\n"
+	resultURL = requests.get("http://inspirobot.me/api?generate=true",stream=False,headers=headerDict).text
 	return resultURL
 
 def getimage(url):
-	# https://stackoverflow.com/questions/13303449/urllib2-httperror-http-error-403-forbidden
 	headerDict = {
 		"User-Agent" : "Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19" 
 	}
-	q = Request(url)
-	q.add_header("User-Agent", "headerDict['User-Agent']")
-	r = urlopen(q)
-	return r
+	picdata = requests.get(url,headers=headerDict)
+	# Create an image file object
+	picture = io.BytesIO(picdata.content)
+	# Use PIL to read formats like jpg, png...
+	picture_pil = Image.open(picture)
+	return picture_pil
 
 # Create new window
 window = tkinter.Tk()
@@ -40,12 +42,8 @@ window.wm_iconbitmap('lightbulb.ico')
 
 # Get the image
 motivational = getimage(inspire())
-# Create an image file object
-picture = io.BytesIO(motivational.read())
-# Use PIL to read formats like jpg, png...
-pil_picture = Image.open(picture)
 # Convert to an image that TKinter can handle
-tk_picture = ImageTk.PhotoImage(pil_picture)
+tk_picture = ImageTk.PhotoImage(motivational)
 # Put the image on a typical widget
 label = tkinter.Label(window, image = tk_picture)
 # Pack (add) it into window
